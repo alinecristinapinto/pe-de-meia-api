4d31992e5141ac2ad8e7ab596138a9a061cc64ee
Aline Cristina <ninanerd15@gmail.com>
2020-04-28 22:48:48 -0300

    Adiciona estrutura do projeto

---
+++ src/main/java/br/com/flourish/pedemeia/PedemeiaApplication.java
@@ -0,0 +1,13 @@
+package br.com.flourish.pedemeia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedemeiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedemeiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/flourish/pedemeia/PedemeiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.flourish.pedemeia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedemeiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
