b9851e064b327c9158f91d0c26c62207223bcda5
Aline Cristina <ninanerd15@gmail.com>
2020-05-01 23:49:28 -0300

    Adiciona service de usuário

--- src/main/java/br/com/flourish/pedemeia/db/sql/pedemeia/repository/UsuarioRepository.java
+++ src/main/java/br/com/flourish/pedemeia/db/sql/pedemeia/repository/UsuarioRepository.java
@@ -3,9 +3,9 @@ package br.com.flourish.pedemeia.db.sql.pedemeia.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.com.flourish.pedemeia.db.sql.pedemeia.entity.PerfilEntity;
+import br.com.flourish.pedemeia.db.sql.pedemeia.entity.UsuarioEntity;
 
 @Repository
-public interface PerfilRepository extends JpaRepository< PerfilEntity, Integer >{
+public interface UsuarioRepository extends JpaRepository< UsuarioEntity, Integer >{
 
 }
